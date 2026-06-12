3b0cd4a03f46113d1597fc4efb98eb7d62aab153
Rama <rizkiramarama@gmail.com>
2026-06-13 03:14:39 +0700

    add SQLite database load script and update gitignore to exclude database files

--- scripts/load.py
+++ scripts/load.py
@@ -1,7 +1,37 @@
-# Loading script
-def load():
-    print("Loading data...")
-    pass
+import sqlite3
+import pandas as pd
+from pathlib import Path
+
+def load_weather_data():
+    csv_path = Path("data/processed/clean_weather_data.csv")
+    database_path = Path("data/weather_data.db")
+
+    if not csv_path.exists():
+        print("file tidak ada")
+        return
+
+    df = pd.read_csv(csv_path)
+
+    connection = sqlite3.connect(database_path)
+
+    df.to_sql(
+        "weather_data",
+        connection,
+        if_exists="replace",
+        index=False
+    )
+
+    total_rows = pd.read_sql_query(
+        "SELECT COUNT(*) AS total_rows FROM weather_data",
+        connection
+    )
+
+    print("Data berhasil dimasukkan ke database.")
+    print(f"Lokasi database: {database_path}")
+    print(total_rows)
+
+    connection.close()
+
 
 if __name__ == "__main__":
-    load()
+    load_weather_data()
