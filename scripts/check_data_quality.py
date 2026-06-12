2cbd7444c34a14d377e0ac45b8055c8baf7081c9
Rama <rizkiramarama@gmail.com>
2026-06-13 03:01:14 +0700

    update data

--- scripts/check_data_quality.py
+++ scripts/check_data_quality.py
@@ -1,34 +1,37 @@
 import pandas as pd
 from pathlib import Path
 
+
 def check_data_quality():
     file_path = Path("data/processed/clean_weather_data.csv")
 
     if not file_path.exists():
-        print("File tidak ada")
+        print("File clean_weather_data.csv belum ditemukan.")
         return
 
     df = pd.read_csv(file_path)
 
-    print("Jumlah baris dan kolom: ")
+    print("Jumlah baris dan kolom:")
     print(df.shape)
 
-    print("\n5 data teratas: ")
+    print("\n5 data teratas:")
     print(df.head())
 
-    print("\nTipe data teratas: ")
+    print("\nTipe data setiap kolom:")
     print(df.dtypes)
 
-    print("\nJumlah data kosong per kolom: ")
+    print("\nJumlah data kosong per kolom:")
     print(df.isnull().sum())
 
-    print("\nJumlah data duplicate: ")
+    print("\nJumlah data duplicate:")
     print(df.duplicated().sum())
 
-print("\nRata-rata suhu per kota:")
-print(df.groupby("city")["temperature_2m"].mean().sort_values(ascending=False))
+    print("\nRata-rata suhu per kota:")
+    print(df.groupby("city")["temperature_2m"].mean().sort_values(ascending=False))
 
-print("\nRata-rata kelembaban per kota:")
-print(df.groupby("city")["relative_humidity_2m"].mean().sort_values(ascending=False))
+    print("\nRata-rata kelembaban per kota:")
+    print(df.groupby("city")["relative_humidity_2m"].mean().sort_values(ascending=False))
+
+
 if __name__ == "__main__":
     check_data_quality()
